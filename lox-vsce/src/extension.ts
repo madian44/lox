bac6955253143fe2da090f85fd1a277bb7795f9e
madian44 <madian44@users.noreply.github.com>
2023-11-07 15:56:07 +0000

    Add scan to command palette

--- lox-vsce/src/extension.ts
+++ lox-vsce/src/extension.ts
@@ -3,6 +3,8 @@
 import * as vscode from 'vscode';
 import * as wasm from '../out/wasm/lox_wasm';
 
+const outputChannel = vscode.window.createOutputChannel("Lox", "lox");
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -14,14 +16,26 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('lox-vsce.helloLox', () => {
+	let helloLox = vscode.commands.registerCommand('lox-vsce.helloLox', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
         wasm.greet();
 		vscode.window.showInformationMessage('Hello World from lox-vsce!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(helloLox);
+
+	let scanLox = vscode.commands.registerCommand('lox-vsce.scanLox', () => {
+		let activeEditor = vscode.window.activeTextEditor;
+		if(!activeEditor) {
+			return;
+		}
+		let contents = activeEditor.document.getText();
+		let response = wasm.scan(contents);
+		outputChannel.append(response);
+	});
+
+	context.subscriptions.push(scanLox);
 }
 
 // This method is called when your extension is deactivated
--- lox-wasm/src/lib.rs
+++ lox-wasm/src/lib.rs
@@ -8,10 +8,15 @@ extern "C" {
 
     #[wasm_bindgen(js_namespace=["window"], js_name=showInformationMessage)]
     fn showInformationMessage(s: &str);
-
 }
 
 #[wasm_bindgen]
 pub fn greet() {
     showInformationMessage("Hello, wasm");
+}
+
+#[wasm_bindgen]
+pub fn scan(text: &str) -> String {
+    showInformationMessage("Hello, wasm");
+    format!("output of a scan {text}")
 }
